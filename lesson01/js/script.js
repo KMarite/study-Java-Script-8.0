686869c250802a7e85537d656b1d76359a6e1617
Marina Kravchenko <kravchenko.marite@gmail.com>
2019-07-01 23:46:22 +0300

    Сделано задание к уроку 2

--- lesson01/js/script.js
+++ lesson01/js/script.js
@@ -1,11 +1,22 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 5000,
+    income = 'Фриланс',
+    addExpenses = 'Интернет, салон, коммуналка',
+    deposit = true,
+    mission = 100000,
+    period = 6;
 
-alert ('Привет привет!');
-console.log ('Надеюсь с заданием справилась:) ');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
 
+console.log('Период ' + period + ' месяца');
+console.log('Цель заработать ' + mission + ' гривен');
 
+addExpenses = addExpenses.toLowerCase().split(", ");
+console.log(addExpenses);
+
+let budgetDay = money/30;
+
+console.log(budgetDay);
+console.log(budgetDay % 2);
