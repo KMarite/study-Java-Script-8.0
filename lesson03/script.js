3f2a47d2a012437d4a0b711c3e77633ca5c3a026
Marina Kravchenko <kravchenko.marite@gmail.com>
2019-07-04 00:27:59 +0300

    Исправлены ошибки в переменной deposit и в проверке budgetDay

--- lesson03/script.js
+++ lesson03/script.js
@@ -3,15 +3,9 @@
 let money = +prompt('Ваш месячный доход?'),
   income = 'Фриланс',
   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-  deposit = prompt('Есть ли у Вас депозит в банке?'),
+  deposit = confirm('Есть ли у Вас депозит в банке?'),
   mission = 100000,
   period = 6;
-
-if (deposit === 'да'){
-  console.log(true);
-} else {
-  console.log(false);
-}
 
 console.log(typeof money);
 console.log(typeof income);
@@ -34,9 +28,9 @@ console.log(Math.floor(budgetDay));
 
 if (budgetDay >= 800){
   console.log('Высокий уровень дохода');
-} else if (budgetDay >= 300 && budgetDay <= 800){
+} else if (budgetDay >= 300 && budgetDay < 800){
   console.log('Средний уровень дохода');
-} else if (budgetDay >= 0 && budgetDay <= 300){
+} else if (budgetDay >= 0 && budgetDay < 300){
   console.log('Низкий уровень дохода');
 } else {
   console.log('Что то пошло не так');
