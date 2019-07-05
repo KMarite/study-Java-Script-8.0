47db2a777fcefaf1a78d2f2cb44954c02a099e6b
Marina Kravchenko <kravchenko.marite@gmail.com>
2019-07-06 01:14:17 +0300

    Сделано обязательное задание к уроку 4

---
+++ lesson04/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let money = +prompt('Ваш месячный доход?'),
+  income = 'Фриланс',
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+  deposit = confirm('Есть ли у Вас депозит в банке?'),
+  mission = 100000,
+  period = 6;
+
+let showTypeof = function(item) {
+  console.log(item, typeof item);
+};
+
+showTypeof(money);
+showTypeof(income);
+showTypeof(deposit);
+
+let questionSpend1 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    expenses1 = +prompt("Во сколько это обойдется?"),
+    questionSpend2 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    expenses2 = +prompt("Во сколько это обойдется?");
+
+
+let getExpensesMonth = function() {
+  return expenses1 + expenses2;
+};
+
+let budgetMonth = money - expenses1 - expenses2;
+let getAccumulatedMonth = function() {
+  return budgetMonth;
+};
+let accumulatedMonth = getAccumulatedMonth();
+console.log('accumulatedMonth:', accumulatedMonth);
+
+
+let goalAchieved = mission / budgetMonth;
+let getTargetMonth = function() {
+  return goalAchieved;
+};
+console.log('getTargetMonth():', Math.floor(getTargetMonth()));
+
+
+let budgetDay = budgetMonth / 30;
+
+function getStatusIncome(){
+  if (budgetDay >= 800) {
+    return('Высокий уровень дохода');
+  } else if (budgetDay >= 300 && budgetDay < 800) {
+    return('Средний уровень дохода');
+  } else if (budgetDay >= 0 && budgetDay < 300) {
+    return('Низкий уровень дохода');
+  } else {
+    return('Что то пошло не так');
+  }
+}
+console.log('getStatusIncome(): ', getStatusIncome());
+
