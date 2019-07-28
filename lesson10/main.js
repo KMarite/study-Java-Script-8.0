65ff44004618ed605aa3f7574c7c95b4cd666dd2
Marina <kravchenko.marite@gmail.com>
2019-07-28 22:01:39 +0300

    созданы файлы для урока 10

---
+++ lesson10/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
+
+DomElement.prototype.createElem = function(){
+    let newElem;
+    if (this.selector[0] === '.'){
+      newElem = document.createElement('div');
+    } else if(this.selector[0] === '#'){
+        newElem = document.createElement('p');
+    } 
+
+    newElem.innerHTML = 'Любой текст';
+    newElem.style.height = this.height + 'px';
+    newElem.style.width = this.width + 'px';
+    newElem.style.background = this.bg;
+    newElem.style.fontSize = this.fontSize + 'px';
+    console.log(newElem);
+};
+
+let block1 = new DomElement('.text', 100, 100, 'green', 14);
+block1.createElem();
+let block2 = new DomElement('#text', 100, 100, 'red', 14);
+block2.createElem();
